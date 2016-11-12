e94d3decdf3da7df5ad52ae011fe252e1dac40ab
Fan Ma <Fan Ma>
2016-11-12 10:34:11 -0500

    moved to progress.cpp and completed core game engine

--- CUESDK/examples/keypress_game/text_highlight/text_highlight.cpp
+++ CUESDK/examples/keypress_game/text_highlight/text_highlight.cpp
@@ -6,7 +6,17 @@
 #include <iostream>
 #include <thread>
 #include <string>
+#include <vector>
+#include <future>
+#include <map>
 
+#include <cstdlib>
+#include <time.h>
+#include <conio.h>
+#include <windows.h>
+
+
+const char* const a_to_z = "abcdefghijklmnopqrstuvwxyz";
 const char* toString(CorsairError error) 
 {
 	switch (error) {
@@ -27,13 +37,61 @@ const char* toString(CorsairError error)
 	}
 }
 
+double getKeyboardWidth(CorsairLedPositions *ledPositions)
+{
+	const auto minmaxLeds = std::minmax_element(ledPositions->pLedPosition, ledPositions->pLedPosition + ledPositions->numberOfLed,
+		[](const CorsairLedPosition &clp1, const CorsairLedPosition &clp2) {
+		return clp1.left < clp2.left;
+	});
+	return minmaxLeds.second->left + minmaxLeds.second->width - minmaxLeds.first->left;
+}
+
 void highlightKey(CorsairLedId ledId)
 {
-	for (auto x = .0; x < 2; x += .1) {
+	for (auto x = .0; x < 2; x += .01) {
 		int val = (1 - abs(x - 1)) * 255;
-		auto ledColor = CorsairLedColor{ ledId, val, val, val };
+		auto ledColor = CorsairLedColor{ ledId, 255, 0, 0 };
 		CorsairSetLedsColors(1, &ledColor);
-		std::this_thread::sleep_for(std::chrono::milliseconds(30));
+		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		ledColor = CorsairLedColor{ ledId, 0, 0, 0 };
+		CorsairSetLedsColors(1, &ledColor);
+	}
+}
+void driftKey(CorsairLedId ledId) {
+	std::map<std::string, int> VK_Keys;
+	VK_Keys["0"] = 0x30;
+	VK_Keys["p"] = 0x50;
+	VK_Keys[";"] = 0xBA;
+	VK_Keys["."] = 0xBE;
+	int musicLine[4][4] = { { 60,40 },{ 80,60 },{ 90,80 },{ 110,90 } };
+	const auto ledPositions = CorsairGetLedPositions();
+	if (ledPositions && ledPositions->numberOfLed > 0) {
+
+		const auto keyboardWidth = getKeyboardWidth(ledPositions) * 1 / 2;
+		const auto numberOfSteps = 50;
+		std::cout << "Working... Press Escape to close program...";
+		std::cout << keyboardWidth;
+		for (auto n = 0; !GetAsyncKeyState(VK_ESCAPE); n++) {
+
+			std::vector<CorsairLedColor> vec;
+			const auto currWidth = double(keyboardWidth) * (n % (numberOfSteps + 1)) / numberOfSteps;
+
+			for (auto i = 0; i < ledPositions->numberOfLed; i++) {
+				const auto ledPos = ledPositions->pLedPosition[i];
+				auto ledColor = CorsairLedColor();
+				ledColor.ledId = ledPos.ledId;
+				if (ledPos.left < currWidth && ledPos.top < musicLine[1][0] && ledPos.top > musicLine[1][1])
+					ledColor.r = 255;
+				vec.push_back(ledColor);
+			}
+			CorsairSetLedsColors(vec.size(), vec.data());
+			if (!GetAsyncKeyState(VK_Keys["p"])) {
+				std::cout << "wrong";
+			} else {
+				std::cout << "right";
+			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		}
 	}
 }
 
@@ -52,7 +110,37 @@ int main()
 	auto userInputStr = std::string();
 	std::cin >> userInputStr;
 		
+	srand(time(NULL));
+	int key = rand() % 26;
+	bool toBreak = false;
+	char pressed;
+	pressed = _getch();
+	const auto ledPositions = CorsairGetLedPositions();
+	while (!toBreak) {
+		int key = rand() % 26;
+			/*if (pressed == 27) {
+				toBreak = true;
+			}*/
+			auto ledId = CorsairGetLedIdForKeyName(a_to_z[key]);
+			if (ledId != CLI_Invalid) highlightKey(ledId);
+			if (false && (pressed = _getch()) == (int)a_to_z[key]) {
+				std::cout << "Great!";
+				//break;
+			}
+			else {
+				std::cout << "Wrong!";
+			}
+		/*	short tabKeyState = GetAsyncKeyState(VK_ESCAPE);
+			if ((1 << 16) & tabKeyState)
+			{
+				std::cout << "ESCAPE!";
+				continue;
+			}*/
+			//std::this_thread::sleep_for(std::chrono::milliseconds(0));
+		}
 	/*for (const auto &symbol : userInputStr) {
+		int key = rand() % 26;
+		std::cout << a_to_z[key];
 		auto ledId = CorsairGetLedIdForKeyName(symbol);
 		if (ledId != CLI_Invalid)
 			highlightKey(ledId);
--- CUESDK/examples/progress/progress/progress.cpp
+++ CUESDK/examples/progress/progress/progress.cpp
@@ -3,12 +3,16 @@
 
 #include "CUESDK.h"
 
+#include <map>
 #include <iostream>
 #include <algorithm>
 #include <thread>
 #include <future>
 #include <vector>
 #include <windows.h>
+
+#include <cstdlib>
+#include <time.h>
 
 const char* toString(CorsairError error) 
 {
@@ -47,29 +51,66 @@ int main()
 		getchar();
 		return -1;
 	}
-
+	/*std::map<std::string, int> VK_Keys;
+	VK_Keys["0"] = 0x30;
+	VK_Keys["p"] = 0x50;
+	VK_Keys["l"] = 0x4C;
+	VK_Keys["."] = 0xBE;*/
+	int VK_Keys[4] = { 0x30 ,0x50,0x4C,0xBE };
+	char ledIds[4] = { '0','p','l','.' };
+	int musicLine[4][4] = { { 60,40 },{ 80,60 },{ 90,80 },{ 120,100 } };
 	const auto ledPositions = CorsairGetLedPositions();
 	if (ledPositions && ledPositions->numberOfLed > 0) {
-				
-		const auto keyboardWidth = getKeyboardWidth(ledPositions);
+
+		int key = 0;
+		bool lineancy = false;
+		srand(time(NULL));
+		const auto keyboardWidth = getKeyboardWidth(ledPositions) * 1 / 2;
 		const auto numberOfSteps = 50;
 		std::cout << "Working... Press Escape to close program...";
+		std::cout << keyboardWidth;
 		for (auto n = 0; !GetAsyncKeyState(VK_ESCAPE); n++) {
+			if ((n % (numberOfSteps + 1)) == 0) {
+				//auto ledColorTarget = CorsairLedColor{ CorsairGetLedIdForKeyName(ledIds[key]), 0, 0, 0 };
+				//CorsairSetLedsColors(1, &ledColorTarget);
+				lineancy = false;
+				key = rand() % 4;
+			}
 
 			std::vector<CorsairLedColor> vec;
+			std::vector<CorsairLedColor> vecTarget;
 			const auto currWidth = double(keyboardWidth) * (n % (numberOfSteps + 1)) / numberOfSteps;
+			
 
 			for (auto i = 0; i < ledPositions->numberOfLed; i++) {
 				const auto ledPos = ledPositions->pLedPosition[i];
 				auto ledColor = CorsairLedColor();
+				
 				ledColor.ledId = ledPos.ledId;
-				if (ledPos.left < currWidth)
-					ledColor.r = 255;
+				if (ledPos.left < currWidth && ledPos.top < musicLine[key][0] && ledPos.top > musicLine[key][1])
+					ledColor.b = 255;
 				vec.push_back(ledColor);
 			}
 			CorsairSetLedsColors(vec.size(), vec.data());
+			auto ledColorTarget = CorsairLedColor{ CorsairGetLedIdForKeyName(ledIds[key]), 0, 255, 0 };
+			CorsairSetLedsColors(1, &ledColorTarget);
+			//ledColor = CorsairLedColor{ ledId, 0, 0, 0 };
+			//CorsairSetLedsColors(1, &ledColor);
 
-			std::this_thread::sleep_for(std::chrono::milliseconds(25));
+			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			if ( (n % (numberOfSteps + 1) <= (numberOfSteps+1)) && (n % (numberOfSteps + 1) > (numberOfSteps-10))) {
+				if (GetAsyncKeyState(VK_Keys[key])) {
+					std::cout << "right";
+					lineancy = true;
+				}
+				else if(!lineancy && n % (numberOfSteps + 1) == (numberOfSteps )){ // wrong if you did not press
+					std::cout << "wrong";
+				}
+			}
+			else if (GetAsyncKeyState(VK_Keys[key])) {//wrong if press out of range
+				std::cout << "wrong";
+			}
+			
 		}
 	}
 	return 0;
