381a34cfc83e7ba8aad1c2334269fcbdbce5d05c
AlexEladas <alexeladas@Alexanders-MacBook-Pro.local>
2016-11-11 23:40:36 -0500

    new branch

---
+++ yhacksAlex/yhacksAlex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  yhacksAlex
+//
+//  Created by Alexander Eladas on 2016-11-11.
+//  Copyright © 2016 Alexander Eladas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
